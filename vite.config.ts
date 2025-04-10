357f8ccac940ec5e571baf6303f2547500506ce3
Yash Narwar <yashnarwar111@gmail.com>
2025-04-10 23:17:47 +0530

    fix: vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
 
 
-
+// https://vite.dev/config/
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
- 
   server: {
     port: 5173,
     proxy: {
-      "/api": {
-        target: "http://localhost:5000",      
+      '/api': {
+        target: 'http://localhost:5000',
       },
-    }
-  }
+    },
+  },
+  build: {
+    outDir: path.resolve(__dirname, 'backend/dist'), // ← THIS sends build files into the backend folder
+    emptyOutDir: true,
+  },
 });
